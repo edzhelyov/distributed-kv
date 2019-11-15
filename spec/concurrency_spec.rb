dbd4d7e063b72cf5995b73ee86c8f894d81e503b
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2019-11-15 13:05:26 +0200

    Fix spec was not concurrent

--- spec/concurrency_spec.rb
+++ spec/concurrency_spec.rb
@@ -8,9 +8,17 @@ describe 'Concurrent writes' do
     client = Client.new 1111
     second_client = Client.new 2222
     client.cmd 'ADD_NODE localhost 2222'
+    sleep 0.3
     
-    client.cmd 'XPUT key 4'
-    second_client.cmd 'PUT key 5'
+    # Comment the Threads :)
+    t1 = Thread.new {
+      client.cmd 'XPUT key 4'
+    }
+    t2 = Thread.new {
+      second_client.cmd 'PUT key 5'
+    }
+    t1.join
+    t2.join
 
     client.cmd('GET key').should eq second_client.cmd('GET key')
   end
