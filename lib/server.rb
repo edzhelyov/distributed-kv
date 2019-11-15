9e20ed8fb36cf55f2690d1ebf2a28d863f0ed25c
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2019-11-15 13:13:35 +0200

    Add Value struct

--- lib/server.rb
+++ lib/server.rb
@@ -19,6 +19,9 @@ require 'irb'
 
 Thread.abort_on_exception = true
 
+class Value < Struct.new(:val, :time)
+end
+
 class Server
   def initialize(address, port, logger)
     @store = Concurrent::Hash.new
@@ -57,19 +60,7 @@ class Server
           end
 
 
-          if ['PUT', 'DEL'].include? command
-            if @delay_sync
-              sleep @delay_sync
-              @delay_sync = nil
-            end
-            @nodes.values.each do |node|
-              if command == 'PUT'
-                node.puts "SYS_PUT #{key} #{value}"
-              elsif command == 'DEL'
-                node.puts "SYS_DEL #{key} #{value}"
-              end
-            end
-          end
+          replicate_command_to_nodes(command, key, value)
 
           client.puts response
         end
@@ -80,10 +71,15 @@ class Server
   def process(command, key, value)
     case command
     when 'PUT', 'SYS_PUT'
-      @store[key] = value
+      store_value(key, value)
       'ok'
     when 'GET'
-      @store.fetch(key, :empty)
+      val = @store[key]
+      if val
+        val.val
+      else
+        :empty
+      end
     when 'DEL', 'SYS_DEL'
       @store.delete key
       'ok'
@@ -105,10 +101,7 @@ class Server
 
         node.puts "SYS_ADD_SELF localhost #{@port}"
 
-        @store.each do |k, v|
-          node.puts "SYS_PUT #{k} #{v}"
-        end
-        @logger.info "Node #{node_name} database synched"
+        sync_data_to_node(node)
         'ok'
       end
     when 'REMOVE_NODE'
@@ -124,9 +117,36 @@ class Server
         @logger.info "Node #{node_name} added as parent"
       end
     when 'XPUT'
-      @store[key] = value
-      @delay_sync = 5
+      store_value(key, value)
+      @delay_sync = 1
       'ok'
+    end
+  end
+
+  def store_value(key, value)
+    @store[key] = Value.new value, Time.now.utc
+  end
+
+  def sync_data_to_node(node)
+    @store.each do |k, v|
+      node.puts "SYS_PUT #{k} #{v.val}"
+    end
+    @logger.info "Node #{node_name} database synched"
+  end
+
+  def replicate_command_to_nodes(command, key, value)
+    if ['PUT', 'XPUT', 'DEL'].include? command
+      if @delay_sync
+        sleep @delay_sync
+        @delay_sync = nil
+      end
+      @nodes.values.each do |node|
+        if command == 'PUT' || command == 'XPUT'
+          node.puts "SYS_PUT #{key} #{value}"
+        elsif command == 'DEL'
+          node.puts "SYS_DEL #{key} #{value}"
+        end
+      end
     end
   end
 
